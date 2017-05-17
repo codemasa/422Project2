380b5f0006ce7cfd0966d0d32941216c55852db3
Talaba Pogrebinsky <pogrebinsky2010@gmail.com>
2017-05-17 16:33:36 -0700

    created 3 test cases and began the framework

--- OnTRCK/app/src/main/java/project2/ontrck/MainActivity.java
+++ OnTRCK/app/src/main/java/project2/ontrck/MainActivity.java
@@ -1,23 +1,57 @@
 package project2.ontrck;
 
-import android.support.v7.app.AppCompatActivity;
+
+import java.lang.*;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import java.util.*;
+import android.content.*;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.main_list_view);
+        listView = (ListView) findViewById(R.id.calendar_list_view);
         String [] menuItems = new String[]{"Search","Normal","Special","Show"};
-
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, menuItems);
         listView.setAdapter(adapter);
-    }
+        ListView myListView = (ListView) findViewById(R.id.calendar_list_view);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(android.widget.AdapterView<?> parent, View view, int position, long id) {
+                Log.d("MainActivity", "ListView item clicked.");
 
-    private ListView listView;
+                if (position == 0) {
+                    Intent myIntent = new Intent(MainActivity.this, SearchPage.class);
+                  //  myIntent.putExtra("key", value); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                }
+                else if(position == 1){
+                    Intent myIntent = new Intent(MainActivity.this, NormalPage.class);
+                    //  myIntent.putExtra("key", value); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                }
+                else if(position == 2){
+                    Intent myIntent = new Intent(MainActivity.this, SpecialPage.class);
+                    //  myIntent.putExtra("key", value); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                }
+                else if(position == 3){
+                    Intent myIntent = new Intent(MainActivity.this, ShowPage.class);
+                    //  myIntent.putExtra("key", value); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                }
+            }
+        });
+    }
 }
---
+++ OnTRCK/app/src/main/java/project2/ontrck/SearchPage.java
@@ -0,0 +1,17 @@
+package project2.ontrck;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by talaba on 5/16/17.
+ */
+
+public class SearchPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_page);
+    }
+}
