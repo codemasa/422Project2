bcfb5bd1331a191af17d54268f0921cb083384a9
Talaba Pogrebinsky <pogrebinsky2010@gmail.com>
2017-05-26 01:27:07 -0700

    removed other uneeded directories

--- Test/app/src/main/java/project2/test/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package project2.test;
-
-import android.support.annotation.LayoutRes;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity {
-
-}
